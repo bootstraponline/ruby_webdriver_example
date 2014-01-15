402e652173c0f31a1e5b0bb6e84c7ebd703d1ecd
bootstraponline <code@bootstraponline.com>
2014-01-15 13:30:24 -0500

    Add more textfield examples

--- lib/spec/gym.rb
+++ lib/spec/gym.rb
@@ -2,7 +2,13 @@ describe 'gymnasium tests' do
   t 'verify homepage exists' do
     gym.goto
     gym.sign_in
-    gym.user_name = 'example user'
+    gym.user_name = 'example user' # type text into textfield
+
+    # check that the text matches what we just entered
+    raise unless gym.user_name_element.value == 'example user'
+    # check that the element exists
+    raise unless gym.user_name?
+
     gym.password = 'password'
     gym.submit_sign_in
   end
